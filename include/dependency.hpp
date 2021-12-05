6692f1745348504711043afbcbb68832416cea2d
Achilles Benetopoulos <abenetopoulos@gmail.com>
2021-12-04 19:00:36 -0800

    Update dependency version structure

--- include/dependency.hpp
+++ include/dependency.hpp
@@ -1,5 +1,7 @@
 #if !defined(DEPENDENCY_H)
 
+#include "semver.hpp"
+
 enum class source_type {
     SOURCE_TYPE_GIT = 0,
     SOURCE_TYPE_UNKNOWN,
@@ -40,15 +42,72 @@ struct package_information {
 };
 
 
+struct version_t {
+    version_type type;
+    string exact;
+
+    string versionRange;
+
+    version_t() {
+        this->type = version_type::VERSION_TYPE_DEFAULT;
+        this->exact = "";
+        this->versionRange = "";
+    }
+
+    void FromString(string v) {
+        if (this->IsExact() || this->IsExactSemVer(v)) {
+            this->exact = v;
+
+            return;
+        }
+
+        this->versionRange = v;
+    }
+
+    bool VersionsMatch(string versionToMatch) {
+        if (!this->exact.empty()) {
+            return this->exact == versionToMatch;
+        }
+
+        semver::version semverVersion{versionToMatch};
+
+        // N.b. ideally we would like to hold a reference to this range instead of converting
+        // from a string to a range every time we want to make a comparison, but the library we're
+        // relying upon for semver is _terrible_, and keeping around a pointer to the range would cause
+        // comparisons with it to enter an infinite loop.
+        // Keeping the value around (as opposed to a pointer) is a non-starter, because the `range` type
+        // does not define a default constructor.
+        // Hence, this mess.
+        auto range = semver::range::detail::range(this->versionRange);
+        return range.satisfies(semverVersion, true);
+    }
+
+    bool IsExact() {
+        return this->type != version_type::VERSION_TYPE_SEMVER;
+    }
+
+    bool IsExactSemVer(string v) {
+        return semver::valid(v);
+    }
+
+    bool Empty() {
+        return exact.empty();
+    }
+};
+
+
 struct input_dependency {
     source_type sourceType;
     string source;
 
-    version_type versionType;
-    string version;
+    version_t specifiedVersion;
 
     bool HasValue() {
-        return !(this->source.empty() && this->version.empty());
+        return !(this->source.empty() && this->specifiedVersion.Empty());
+    }
+
+    string GetExactVersion() {
+        return this->specifiedVersion.exact;
     }
 };
 
